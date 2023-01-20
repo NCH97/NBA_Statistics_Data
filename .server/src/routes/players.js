9ac8fcbe0d5337231d954c8fe40cfdf0318001f1
ChNZ <christopher.nze-nong@cityviz.io>
2023-01-20 11:37:34 +0100

    #deletesomething

--- .server/src/routes/players.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { Player } = require('../models/playerModel');
-
-router.get('/', async (req, res) => {
-    const players = await Player.find()
-    res.send(players)
-})
-
-module.exports = router;
--- .server/src/routes/team.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { Team } = require('../models/teamModel');
-
-router.get('/', async (req, res) => {
-    const teams = await Team.find()
-    res.send(teams)
-})
-
-module.exports = router;
